f432e87c9f45308eeb5c04d79aebcf7020c40962
Timur <emelyanov_timur@mail.ru>
2021-02-07 21:59:42 +0300

    Homework for Lesson6

---
+++ les_7_hw1.py
@@ -0,0 +1,39 @@
+"""
+1. Реализовать класс Matrix (матрица). Обеспечить перегрузку конструктора класса (метод __init__()),
+который должен принимать данные (список списков) для формирования матрицы.
+Подсказка: матрица — система некоторых математических величин, расположенных в виде прямоугольной схемы.
+Примеры матриц вы найдете в методичке.
+Следующий шаг — реализовать перегрузку метода __str__() для вывода матрицы в привычном виде.
+Далее реализовать перегрузку метода __add__() для реализации операции сложения двух объектов класса Matrix
+(двух матриц). Результатом сложения должна быть новая матрица.
+Подсказка: сложение элементов матриц выполнять поэлементно — первый элемент первой строки первой матрицы складываем
+с первым элементом первой строки второй матрицы и т.д.
+"""
+
+
+class Matrix:
+    def __init__(self, input_data):
+        self.input = input_data
+
+    def __str__(self):
+        return '\n'.join([' '.join([str(elem) for elem in line]) for line in self.input])
+
+    def __add__(self, other):
+        answer = ''
+        if len(self.input) == len(other.input):
+            for line_1, line_2 in zip(self.input, other.input):
+                if len(line_1) != len(line_2):
+                    return 'Problems with shape'
+
+                summed_line = [x + y for x, y in zip(line_1, line_2)]
+                answer += ' '.join([str(i) for i in summed_line]) + '\n'
+        else:
+            return 'Problems with shape'
+        return answer
+
+
+matrix_1 = Matrix([[1, 2], [3, 4], [5, 6], [7, 8]])
+matrix_2 = Matrix([[2, 3], [4, 5], [6, 7], [10, 20]])
+print(matrix_1)
+print()
+print(matrix_1 + matrix_2)
